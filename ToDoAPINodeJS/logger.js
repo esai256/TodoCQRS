5ecd4905043127e612144f974ffc2ac867b3e88b
Elias Seichter <esai.n900@gmail.com>
2017-12-01 22:19:00 +0100

    Make the logger-class write into a file. Todo: implement in todoCQRS

--- ToDoAPINodeJS/logger.js
+++ ToDoAPINodeJS/logger.js
@@ -1,29 +1,49 @@
-module.exports = class Logger {
-    constructor(logFile = `${__dirname}/todoCQRS.log`) {
-        this.LogFile = logFile;
+const fs = require("fs");
+
+module.exports = class Logger
+{
+    constructor(logFileName = `${__dirname}/todoCQRS.log`)
+    {
+        this.logFileName = logFileName;
     }
-    
-    log(message) {
-        let logMessage = `${new Date()} || Info: ${message}`;
+
+    test() {
+        this.log("test1");
+        this.warn("test2");
+        this.error("test3");
+    }
+
+    log(message)
+    {
+        let logMessage = `${getCurrentTimeStamp()}\t || Info    ||\t ${message}\n`;
 
         console.log(logMessage);
 
-        // this.LogFile.write(logMessage);
+        fs.appendFile(this.logFileName, logMessage, error => error ? console.error(`Could not write to the logs! ${error}`) : undefined);
     }
 
-    warn(message) {
-        let logMessage = `${new Date()} || Warning: ${message}`;
+    warn(message)
+    {
+        let logMessage = `${getCurrentTimeStamp()}\t || Warning ||\t ${message}\n`;
 
         console.warn(logMessage);
 
-        // this.LogFile.write(logMessage);
+        fs.appendFile(this.logFileName, logMessage, error => error ? console.error(`Could not write to the logs! ${error}`) : undefined);
     }
 
-    error() {
-        let logMessage = `${new Date()} || Error: ${message}`;
+    error(message)
+    {
+        let logMessage = `${getCurrentTimeStamp()}\t || Error   ||\t ${message}\n`;
 
         console.error(logMessage);
 
-        // this.LogFile.write(logMessage);
+        fs.appendFile(this.logFileName, logMessage, error => error ? console.error(`Could not write to the logs! ${error}`) : undefined);
     }
 };
+
+function getCurrentTimeStamp(date = new Date(Date.now()))
+{
+    const monthOffset = 1;
+
+    return `${date.getFullYear()}-${date.getMonth()+monthOffset}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`;
+}
